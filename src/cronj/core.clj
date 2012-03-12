6f13ea15e32a3021ec71760c5d74301d28186d64
Maximilian Karasz <maximilian.karasz@googlemail.com>
2012-03-12 22:50:35 +0100

    removed duplicate protocol definitions

--- src/cronj/core.clj
+++ src/cronj/core.clj
@@ -2,8 +2,3 @@
   (:import [java.util.concurrent PriorityBlockingQueue]
            [java.lang Comparable]))
 
-(defprotocol Periodic
-  (next-event [this seed]))
-
-(defprotocol Displayable
-  (display [this]))
